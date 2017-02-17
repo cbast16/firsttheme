d2c9cb20c24407745e4d4164abfdbb192b168bfb
cbast16 <calvin.bast@gmail.com>
2017-02-17 15:26:12 -0600

    finished index

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" <?php echo get_bloginfo( 'description' ); ?>
-    <meta name="author" content="">
+    <meta name="description" <?php echo get_bloginfo( 'description' ); ?>>
+    
 
     <title><?php echo get_bloginfo( 'name' ); ?></title>
 
@@ -14,7 +14,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -31,16 +31,9 @@
       <div class="container">
         <nav class="blog-nav">
           <a class="blog-nav-item active" href="#">Home</a>
-          <?php wp_list_pages( '&title_li=' ); ?>
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav blog-nav' ) );?>
         </nav>
       </div>
     </div>
 
-    <div class="container">
-
-      <div class="blog-header">
-	   <h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-	   <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
-      </div>
-        
-    </div><!-- /.container -->
+    
--- index.php
+++ index.php
@@ -1,35 +1,41 @@
 <?php get_header(); ?>
 
-<div class="demo-card-square mdl-card mdl-shadow--2dp">
-  <div class="mdl-card__title mdl-card--expand">
-    <h2 class="mdl-card__title-text">Update</h2>
-  </div>
-  <div class="mdl-card__supporting-text">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Aenan convallis.
-  </div>
-  <div class="mdl-card__actions mdl-card--border">
-    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-      View Updates
-    </a>
-  </div>
+<div class="wrapper">
+    
+  <img src="wp-content/themes/firsttheme/img/shield.png" class="shield">
+        
+
+      <div class="sections">
+        
+        <div class="subSecs">  
+            <a href="favorites.html"><h2>Favorites</h2></a>
+            <p>
+                These are my favorite beers made by Sam Adams, Boston Brewing Co.
+            </p>
+        </div>
+          
+        <div class="subSecs">
+            <a href="seasonals.html"><h2>Current Seasonal Beers</h2></a>
+            <p>
+                Sam Adams offers multiple beers for every season, including a seasonal pack. 
+            </p>
+        </div>
+          
+        <div class="subSecs">
+            <a href="samandme.html"><h2>Sam and Me!</h2></a>
+            <p>
+                A collection of images of how I enjoy my Sam's!
+            </p>
+        </div>
+    </div>
+  
 </div>
+  
 
-<div class="row">
 
-        <div class="col-sm-8 blog-main">
-            <?php
-            if ( have_posts() ) : while ( have_posts() ) : the_post();
-            
-                get_template_part( 'content', get_post_format() ); 
-            
-            
-            endwhile; endif;
-            ?>
-        </div><!-- /.blog-main -->
 
-        <?php get_sidebar(); ?>
 
-</div><!-- /.row -->
+
+
 
 <?php get_footer(); ?>
